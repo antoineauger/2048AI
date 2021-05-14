4575f9531743070b94039f1082c98271f9043ff0
Antoine Auger <antoineauger@users.noreply.github.com>
2021-05-14 17:19:39 +0200

    Command line arguments + documentation updated (part 4)

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 # coding: utf-8
+import argparse
+import pathlib
 from os import path
 from random import shuffle
-
 import numpy as np
 import Constants
 from Constants import Directions
@@ -9,47 +10,72 @@ from ai.Layer import Layer
 from ai.NeuralNetwork import NeuralNetwork
 from model.Game import Game
 from model.Grid import Grid
+from ui.Window import Window
 
-# We create a new empty Grid object
-grid = Grid(nb_rows_columns=Constants.GRID_NB_ROWS_COLUMNS)
-# We create a Game from that Grid with two tiles to start
-game = Game(grid, init_grid_with_two_tiles=True)
+# Parser for command line arguments
+my_parser = argparse.ArgumentParser(description='Play a new 2048 game or analyze a finished one', allow_abbrev=False)
+my_parser.add_argument('mode', action='store', type=str, choices=['PLAY', 'STATS'],
+                       help='whether to play or analyze a 2048 game')
+my_parser.add_argument('--game', action='store', type=str, choices=['HUMAN', 'RANDOM', 'NEURAL'],
+                       help='the kind of 2048 game to play')
+my_parser.add_argument('--path', action='store', type=pathlib.Path,
+                       help='relative path of the game log file to analyze in the data folder '
+                            '(e.g., train_logs/human_2048_1.log)')
+args = my_parser.parse_args()
 
-# The list of directions that can be played
-directions = [Directions.LEFT, Directions.RIGHT, Directions.UP, Directions.DOWN]
+# Additional checks for parser
+if args.mode == 'PLAY' and (args.game is None):
+    my_parser.error("PLAY mode requires the --game argument to be given.")
+elif args.mode == 'STATS' and (args.path is None):
+    my_parser.error("STATS mode requires the --path argument to be given.")
 
-# You can choose between RANDOM, NEURAL and STATS modes
-mode = "NEURAL"
+# --------------- PLAY mode ---------------
+if args.mode == 'PLAY':
+    # We create a new empty Grid object
+    grid = Grid(nb_rows_columns=Constants.GRID_NB_ROWS_COLUMNS)
+    # We create a Game from that Grid with two tiles to start
+    game = Game(grid, init_grid_with_two_tiles=True)
 
+    replay_dir = path.join(Constants.DATA_DIR_NAME,
+                           'replays',
+                           '{}_{}'.format(Constants.GRID_NB_ROWS_COLUMNS, Constants.GRID_NB_ROWS_COLUMNS))
 
-if mode == "RANDOM":  # --------------- RANDOM test ---------------
-    while not game.ended_game:  # While the game is not finished
-        shuffle(directions)
-        game.play_many_directions(directions)  # We played one of the four directions
+    # --------------- RANDOM game ---------------
+    if args.game == "RANDOM":
+        # The list of directions that can be played
+        directions = [Directions.LEFT, Directions.RIGHT, Directions.UP, Directions.DOWN]
+        while not game.ended_game:  # While the game is not finished
+            shuffle(directions)
+            game.play_many_directions(directions)  # We played one of the four directions
+        game.save_game(base_path=replay_dir)
 
-elif mode == "NEURAL":  # --------------- NEURAL test ---------------
-    nn = NeuralNetwork()  # We create our neural network
-    nn.add_layer(Layer(16, 4))  # Only one hidden layer
-    nn.add_layer(Layer(4, 4))  # Output layer
+    # --------------- NEURAL game ---------------
+    elif args.game == "NEURAL":
+        # TODO: customize your neural network below
+        nn = NeuralNetwork()
+        nn.add_layer(Layer(16, 4))  # Only one hidden layer
+        nn.add_layer(Layer(4, 4))  # Output layer
+        # End of neural network customization
 
-    train_dir = path.join(Constants.DATA_DIR_NAME, Constants.TRAIN_DIR_NAME)
-    nn.train_from_directory(directory=train_dir,
-                            learning_rate=Constants.NEURAL_NET_TRAINING_RATE,
-                            max_epochs=Constants.NEURAL_NET_MAX_EPOCHS)
+        train_dir = path.join(Constants.DATA_DIR_NAME, Constants.TRAIN_DIR_NAME)
+        nn.train_from_directory(directory=train_dir,
+                                learning_rate=Constants.NEURAL_NET_TRAINING_RATE,
+                                max_epochs=Constants.NEURAL_NET_MAX_EPOCHS)
 
-    while not game.ended_game:
-        x = np.array(np.mat(game.grid.to_string()))
-        predictions = nn.predict(x)
-        game.play_many_directions(predictions)
+        while not game.ended_game:
+            x = np.array(np.mat(game.grid.to_string()))
+            predictions = nn.predict(x)
+            game.play_many_directions(predictions)
+        game.save_game(base_path=replay_dir)
 
-elif mode == "STATS":  # --------------- STATS only ---------------
-    filepath = path.join(Constants.DATA_DIR_NAME, 'train_logs', 'human_2048_1.log')
+    # --------------- HUMAN game ---------------
+    else:
+        # No need to create grid or game objects when we launch GUI
+        app = Window(nb_rows_columns=Constants.GRID_NB_ROWS_COLUMNS, base_path=replay_dir)
+        app.start_new_game()
+
+# --------------- STATS mode ---------------
+else:
+    filepath = path.join(Constants.DATA_DIR_NAME, args.path)
     game = Game.load_game(filepath, display_grid=False)
-
-# Finally, we save the game in the replay directory if mode was RANDOM or NEURAL
-replay_dir = path.join(Constants.DATA_DIR_NAME,
-                       'replays',
-                       '{}_{}'.format(Constants.GRID_NB_ROWS_COLUMNS, Constants.GRID_NB_ROWS_COLUMNS))
-if mode != "STATS":
-    game.save_game(base_path=replay_dir)
-game.history.print_stats()
+    game.history.print_stats()
--- README.md
+++ README.md
@@ -31,7 +31,6 @@ If you want more details, please refer to the series of blog posts that I wrote 
 │   │   TkConstants.py
 │   │   Window.py
 │
-│   Application.py
 │   Constants.py
 │   Main.py
 ```
@@ -42,20 +41,38 @@ If you want more details, please refer to the series of blog posts that I wrote 
 * numpy
 * tkinter
 
-## I am human, how to play?
+## Where to start?
 
-The  entry point of the program is the `Main.py` file.
-Add/comment all the logic you want to execute there. 
+The  entry point of the program is the `Main.py` file. To display the program usage help, simply type:
+```
+$ python3 Main.py -h
+```
+Result:
+```
+usage: Main.py [-h] [--game {HUMAN,RANDOM,NEURAL}] [--path PATH] {PLAY,STATS}
 
-For the sake of simplicity, some interesting variables are defined in the `Constants.py` file.
+Play a new 2048 game or analyze a finished one
+
+positional arguments:
+  {PLAY,STATS}          whether to play or analyze a 2048 game
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --game {HUMAN,RANDOM,NEURAL}
+                        the kind of 2048 game to play
+  --path PATH           relative path of the game log file to analyze in the
+                        data folder (e.g., train_logs/human_2048_1.log)
+```
+
+For the sake of simplicity, some variables are defined in the `Constants.py` file.
 By editing this file, you can easily change:
 * The number of rows and columns for a squared Grid (default: `4`);
 * The tile value required to win a game (default: `2048`);
 * The root directory where to save game logs (default: `data`).
 
-Then, to launch the game in an interactive mode with GUI, simply type:
+Then, to play a new game in an interactive mode with GUI, simply type the following command in your terminal:
 ```
-$ python3 Application.py
+$ python3 Main.py PLAY --game HUMAN
 ```
 
 ![2048_gui](/assets/gui_play_mode.png?raw=true "2048 GUI play mode")
@@ -68,8 +85,11 @@ For instance, all the game logs corresponding to a 4x4 grid size will be saved i
 
 ## How to use the replay mode?
 
-Replay mode is only available through the GUI.
-You can import a previously saved 2048-game log file by clicking on `File > Open game...`.
+Replay mode is only available through the GUI. To start the GUI, enter the command:
+```
+$ python3 Main.py PLAY --game HUMAN
+```
+You can then import a previously saved 2048-game log file by clicking on `File > Open game...`.
 
 Then, you can navigate through the game with the directional arrows ( &larr; &rarr; &uarr; &darr; ).
 
@@ -79,13 +99,13 @@ Then, you can navigate through the game with the directional arrows ( &larr; &ra
 
 1. Open the file `Main.py` and design your own Neural Network below the line:
 ```
-elif mode == "NEURAL":  # --------------- NEURAL test ---------------
+# TODO: customize your neural network below
 ```
 You can customize the number of layers (with `add_layer`) as well as the number of neurons 
 within each layer (`(Layer(X, Y)`).
 
 Finally, you can also customize the directory used for training (`train_logs` by default), the learning rate (`0.3` by default) as well as the number of 
-cycles that the training process should last (`400` by default). 
+cycles that the training process should last (`400` by default).
 
 For convenience, all these parameters can be edited in the `Constants.py` file:
 ```
@@ -100,23 +120,17 @@ implement a flexible neural network with backpropagation from scratch.
    Make sure that the logs match the dimensions of the Neural Network you want to train.
 
 
-3. Enable neural mode in the `Main.py` file:
+3. From your terminal, start the program with the following command:
 ```
-# You can choose between RANDOM, NEURAL and STATS modes
-mode = "NEURAL"
-```
-
-4. Ask your AI to automatically play a 2048 game:
-```
-$ python3 Application.py
+$ python3 Main.py PLAY --game NEURAL
 ```
 
-5. After training, your AI will play until it wins/looses (spoiler: it is most likely to 
+4. After training, your AI will play until it wins/looses (spoiler: it is most likely to 
    loose :stuck_out_tongue_winking_eye:).
    When it does, some game stats are displayed before the program ends. 
    
 If you want to precisely see which moves your AI played (although it will not tell you why it decided 
-to play that way/moove), the associated replay is available in `data/replays/`.
+to play that way/move), the associated replay is available in `data/replays/`.
 
 ```
 OK - File parsed: data\train_logs\human_1024_1.log
@@ -145,3 +159,26 @@ scope of this project :grin:.
 
 Nevertheless, you can read my blog post to understand better why your
 AI is (almost) always loosing if you play with a high-target tile (such as 2048).
+
+## How to automatically play a random game?
+
+Simply type the following command in your terminal:
+```
+$ python3 Main.py PLAY --game RANDOM
+```
+
+## How to compute key metrics for a 2048 saved game?
+
+Use the STATS mode alongside with the filepath of the 2048 log that you want to analyze. For instance:
+```
+$ python3 Main.py STATS --path train_logs/human_2048_1.log
+```
+Result:
+```
+Final direction/state: States.WIN
+Final score: 19984
+Number of rounds: 753
+Max. tile: 2048
+Avg. points per round: 26.574468085106382
+Choice frequencies: {0: 100.0}
+```
